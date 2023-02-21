4eecbe4bd2a9fb46a73c045a6360d2af3b434e27
sailendrachettri <sailendra9083@gmail.com>
2023-02-21 18:34:16 +0530

    dark mode toggle

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 
 function App() {
   return (
     <>
       <Navbar title="Tanalyzer" /> {/*  here about props will set fron defaultProps*/}
-      <TextForm />
+      {/* <TextForm /> */}
+      <About />
     </>
   );
 }
--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -13,6 +13,9 @@ export default function Navbar(props) {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
+                                <a className="nav-link" rel="noreferrer" target="_blank" href="/about">About</a>
+                            </li>
+                            <li className="nav-item">
                                 <a className="nav-link" rel="noreferrer" target="_blank" href="https://sdweather.netlify.app">Weather</a>
                             </li>
                             <li className="nav-item">
